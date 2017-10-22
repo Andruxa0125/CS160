cd5a28bc447b33750f95dbca5d3593b3d82c02c2
Rodion Yaryy <ryaryy.zt@zenefits.com>
2017-10-22 13:09:16 -0700

    trying to fix path issues

--- backend/facefinder/admin.py
+++ backend/facefinder/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Video
+from .models import Video
 
 admin.site.register(Video)
 # Register your models here.
