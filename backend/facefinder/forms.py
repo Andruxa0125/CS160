76b728e6422c97735e7101c7dbf866d87e807544
jaepark124 <jae.park0124@gmail.com>
2017-11-08 18:47:13 -0800

    Changed tables to CSS

--- backend/facefinder/forms.py
+++ backend/facefinder/forms.py
@@ -13,12 +13,20 @@ from .models import Video
 
 # If you don't do this you cannot use Bootstrap CSS
 class LoginForm(AuthenticationForm):
+    firstname = forms.CharField(label="", required=False,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'first_name','placeholder': 'First Name (Optional)'}))
+    lastname = forms.CharField(label="", required=False,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'last_name','placeholder': 'Last Name (Optional)'}))
     username = forms.CharField(label="",
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username','placeholder': 'Username'}))
     password = forms.CharField(label="",
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password','placeholder': 'Password'}))
 
 class SignUpForm(UserCreationForm):
+    firstname = forms.CharField(label="", required=False,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'first_name','placeholder': 'First Name (optional)'}))
+    lastname = forms.CharField(label="", required=False,
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'last_name','placeholder': 'Last Name (optional)'}))
     username = EmailField(label=(""), required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'name': 'email','placeholder': 'Email Address'}))
     password1 = forms.CharField(label="",
                     widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password1','placeholder': 'Create a password'}))
@@ -27,7 +35,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
+        fields = ('firstname', 'lastname', 'username', 'password1', 'password2')
 
 class NewVideoForm(forms.ModelForm):
 
