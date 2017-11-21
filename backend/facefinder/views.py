0e905cbb0f6d7974a811fce137386a4c0d7a3a74
jaepark124 <jaeho.park@karfarm.com>
2017-11-20 19:58:47 -0800

    fixed error with neg session id

--- backend/facefinder/views.py
+++ backend/facefinder/views.py
@@ -59,7 +59,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('main', request.user.username, hash(hash(request.user.username)))
+            return redirect('main', request.user.username, abs(hash(hash(request.user.username))))
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
